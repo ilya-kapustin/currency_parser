360a87e5a0aff19178fc692a2cde7e1c720b3010
ilya <ilya.kapustini@mail.ru>
2021-09-28 00:37:21 +0300

    start parser

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+import csv
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-print('Введите дату в формате хх.хх.ххх (28.09.2021)')
-date = input('Дата ')
-url = f'httpswww.cbr.rucurrency_basedailyUniDbQuery.Posted=True&UniDbQuery.To={date}'
+url = 'https://www.cbr.ru/currency_base/daily/?UniDbQuery.Posted=True&UniDbQuery.To=28.09.2021'
 
 source = requests.get(url)
 main_text = source.text
-soup = BeautifulSoup(main_text, features='html.parser')
 
-table = soup.find('table', {'class' 'data'})
-tr = table.findAll('tr')
+soup = BeautifulSoup(main_text, features="html.parser")
 
-for line in tr[1]:
-    print(line.text)
+table = soup.find('table', {'class': 'data'})
+tr = soup.findAll('tr')
+th = soup.findAll('th')
 
-for line in tr[1]:
-    print(line.text.splitlines()[1])
+all_line = []
+for line in tr:
+        all_line.append(line.text.splitlines()[1:])
+
+df_main = pd.DataFrame(all_line)
+df_main.index = df_main[3]
+df_main.columns = df_main.iloc[0]
+df_main = df_main.drop(df_main.index[0])
+df_main = df_main.drop('Валюта', 1)
+
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+print(df_main)

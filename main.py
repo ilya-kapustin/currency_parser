6077e477f268e319a9708c62ddff9784b518fa22
ilya <ilya.kapustini@mail.ru>
2021-10-14 22:54:20 +0300

    start parser

--- main.py
+++ main.py
@@ -1,31 +1,71 @@
+from datetime import datetime, timedelta
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.cbr.ru/currency_base/daily/?UniDbQuery.Posted=True&UniDbQuery.To=28.09.2021'
 
-source = requests.get(url)
-main_text = source.text
+class AllCurrency:
+    def __init__(self, start_date, finished_date, save_name):
+        self.all_line = []
+        self.url = 'https://www.cbr.ru/currency_base/daily/?UniDbQuery.Posted=True&UniDbQuery.To='
+        self.start_date = start_date
+        self.url_now = None
+        self.save_name = save_name
+        self.finished_date = finished_date
 
-soup = BeautifulSoup(main_text, features="html.parser")
+    def settings_views(self):
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', None)
+        pd.set_option('display.width', None)
+        pd.set_option('display.max_colwidth', None)
 
-table = soup.find('table', {'class': 'data'})
-tr = soup.findAll('tr')
-th = soup.findAll('th')
+    def settings_date(self, date):
+        date = datetime.strptime(date, '%d.%m.%Y')
+        return date
 
-all_line = []
-for line in tr:
-        all_line.append(line.text.splitlines()[1:])
+    def settings_url(self, url):
+        source = requests.get(url)
+        main_text = source.text
+        soup = BeautifulSoup(main_text, features="html.parser")
+        table = soup.find('table', {'class': 'data'})
+        self.tr = soup.findAll('tr')
 
-df_main = pd.DataFrame(all_line)
-df_main.index = df_main[3]
-df_main.columns = df_main.iloc[0]
-df_main = df_main.drop(df_main.index[0])
-df_main = df_main.drop('Валюта', axis=1)
+    def run(self):
+        self.settings_views()
+
+        self.url_now = self.url + self.start_date
+        self.settings_url(self.url_now)
+        print(self.url_now)
+
+        self.start_date = self.settings_date(date=self.start_date)
+        self.finished_date = self.settings_date(date=self.finished_date)
+
+        for line in self.tr[:1]:
+            self.all_line.append(line.text.splitlines()[1:])
+            print(line)
+
+        while True:
+            self.start_date = self.start_date + timedelta(days=1)
+            self.start_date = self.start_date.strftime("%d.%m.%Y")
+            self.url_now = self.url + self.start_date
+            self.settings_url(self.url_now)
+            print(self.url_now)
+            self.start_date = self.settings_date(date=self.start_date)
+            for line in self.tr[1:]:
+                if '<td>Украинских карбованецев</td>' not in str(line):
+                    self.all_line.append(line.text.splitlines()[1:])
+            if str(self.finished_date) == str(self.start_date):
+                break
+        self.clean_data()
+
+    def clean_data(self):
+        data = pd.DataFrame(self.all_line)
+        data.columns = data.iloc[0]
+        data = data.drop(data.index[0])
+        data = data.drop('Валюта', axis=1)
+        data.to_csv(self.save_name, encoding='utf-8')
+        print(data, data.columns)
 
 
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
-print(df_main)
+if __name__ == '__main__':
+    AllCurrency('01.07.1992', '14.10.2021', 'data').run()
